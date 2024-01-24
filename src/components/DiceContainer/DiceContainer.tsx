9c02e234f6ff8756686273460d10ee601ef9a151
henry.sweat <hi@henrysweat.com>
2024-01-24 13:51:34 -0500

    Broke out die component

--- src/components/DiceContainer/DiceContainer.tsx
+++ src/components/DiceContainer/DiceContainer.tsx
@@ -1,43 +1,14 @@
-import useGameStateStore, { useGameActions } from '@/stores/gameState';
+import Die from './Die';
 import styles from './DiceContainer.module.css';
 
 export default function DiceContainer() {
-  const rollCounter = useGameStateStore((state) => state.rollCounter);
-  const dice = useGameStateStore((state) => state.dice);
-
-  const { updateDiceStateForDieClicked } = useGameActions();
-
-  function handleDieClicked(e) {
-    if (rollCounter === 3 || rollCounter === 0) {
-      return;
-    }
-    const indexOfClickedDie = e.target.id.slice(4) - 1;
-    updateDiceStateForDieClicked(indexOfClickedDie);
-  }
+  const diceStateIndices = [0, 1, 2, 3, 4];
 
   return (
     <div className={styles.dice}>
-      {dice.map(({ id, value, isSelected }) => (
-        <Die
-          key={id}
-          id={id}
-          value={value}
-          isSelected={isSelected}
-          handleDieClicked={handleDieClicked}
-        />
+      {diceStateIndices.map((diceStateIndex) => (
+        <Die key={`die-${diceStateIndex}`} diceStateIndex={diceStateIndex} />
       ))}
     </div>
-  );
-}
-
-function Die({ id, value, isSelected, handleDieClicked }) {
-  return (
-    <button
-      id={id}
-      className={`${styles.die} ${isSelected ? styles.selected : ''}`}
-      onClick={handleDieClicked}
-    >
-      {value}
-    </button>
   );
 }
---
+++ src/components/DiceContainer/Die.tsx
@@ -0,0 +1,28 @@
+import useGameStateStore, { useGameActions } from '@/stores/gameState';
+import styles from './DiceContainer.module.css';
+
+export default function Die({ diceStateIndex }) {
+  const rollCounter = useGameStateStore((state) => state.rollCounter);
+  const dice = useGameStateStore((state) => state.dice);
+  const { updateDiceStateForDieClicked } = useGameActions();
+
+  const { id, isSelected, value } = dice[diceStateIndex];
+
+  function handleDieClicked(e) {
+    if (rollCounter === 3 || rollCounter === 0) {
+      return;
+    }
+    const indexOfClickedDie = e.target.id.slice(4) - 1;
+    updateDiceStateForDieClicked(indexOfClickedDie);
+  }
+
+  return (
+    <button
+      id={id}
+      className={`${styles.die} ${isSelected ? styles.selected : ''}`}
+      onClick={handleDieClicked}
+    >
+      {value}
+    </button>
+  );
+}
