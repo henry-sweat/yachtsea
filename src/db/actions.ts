65be39a36840823774b9313d1187b49557a85292
henry.sweat <hi@henrysweat.com>
2024-01-29 15:40:23 -0500

    added database and actions

---
+++ src/db/actions.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import { sql } from '@vercel/postgres';
+
+export async function startGame(user_id: string) {
+  try {
+    const game = await sql`INSERT INTO games (user_id, total_score, game_is_finished)
+    VALUES (${user_id}, ${0}, ${false})
+    ON CONFLICT (id) DO NOTHING;`;
+    return game;
+  } catch (error) {
+    console.error('Failed to start game:', error);
+    throw new Error('Failed to start game.');
+  }
+}
+
+export async function endGame(user_id: string, total_score: number) {
+  try {
+    const game = await sql`UPDATE games
+    SET total_score = ${total_score},
+    game_is_finished = ${true}
+    WHERE id = (
+        SELECT id
+        FROM games
+        WHERE user_id = ${user_id}
+        ORDER BY start_date DESC
+        LIMIT 1
+    );
+    `;
+    return game;
+  } catch (error) {
+    console.error('Failed to end game:', error);
+    throw new Error('Failed to end game.');
+  }
+}
